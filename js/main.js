9fb57e35b8b7e6bf5889349ad78e9d54f4f70bba
314ronin <pownmymail@gmail.com>
2021-05-21 07:21:15 +0300

    fix: Add ghLink forEach loop

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const ghLink = document.querySelectorAll('.contacts-entry--github');
-ghLink.addEventListener('click', () => {
-	gtag('event', 'github', {'event_category' : 'link',});
-	console.log('gh clicked');
+const ghLink = document.querySelectorAll('.contacts-entry--github a');
+ghLink.forEach(link => {
+	link.addEventListener('click', () => {
+		gtag('event', 'github', {'event_category' : 'link',});
+		console.log('gh clicked');
+	});
 });
